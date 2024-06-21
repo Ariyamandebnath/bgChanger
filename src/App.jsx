ed4edcee3f5b43662bc8dd259cb9f8671386a390
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-06-21 15:55:25 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,51 +10,77 @@ function App() {
                     style={{ boxShadow: "0 10px 15px rgba(0, 0, 0, 0.5)" }}>
                     <button
                         onClick={() => setColor("red")}
-                        className="outline-none px-4 py-1 rounded-full text-white shadow-2xl border-2 border-black" style={{ backgroundColor: "red" }}>
+                        className="outline-none px-4 py-1 rounded-full text-white shadow-2xl border-2 border-black" style={{
+                            backgroundColor: "red",
+                             boxShadow: "0 6px 5px rgba(0, 0, 0, 0.5)" 
+                        }}>
                         Red
                     </button>
                     <button
                         onClick={() => setColor("green")}
                         className="outline-none px-4 py-1 rounded-full text-white shadow-2xl border-2 border-black"
-                        style={{ backgroundColor: "green" }}
+                        style={{
+                            backgroundColor: "green",
+                            boxShadow: "0 6px 5px rgba(0, 0, 0, 0.5)" 
+                         }}
                     >
                         Green
                     </button>
                     <button
                         onClick={() => setColor("blue")}
                         className="outline-none px-4 py-1 rounded-full text-white shadow-2xl border-2 border-black"
-                        style={{ backgroundColor: "blue" }}
+                        style={{
+                            backgroundColor: "blue",
+                            boxShadow: "0 6px 5px rgba(0, 0, 0, 0.5)" 
+                        }}
                     >
                         Blue
                     </button>
                     <button
                         onClick={() => setColor("yellow")}
                         className="outline-none px-4 py-1 rounded-full text-black shadow-2xl border-2 border-black"
-                        style={{ backgroundColor: "yellow" }}
+                        style={{
+                            backgroundColor: "yellow",
+                            boxShadow: "0 6px 5px rgba(0, 0, 0, 0.5)" 
+                         }}
                     >
                         Yellow
                     </button><button
                         onClick={() => setColor("#E97451")}
                         className="outline-none px-4 py-1 rounded-full text-black shadow-2xl border-2 border-black"
-                        style={{ backgroundColor: "#E97451" }}
+                        style={{
+                            backgroundColor: "#E97451", 
+                            boxShadow: "0 6px 5px rgba(0, 0, 0, 0.5)" 
+                         }}
                     >
                         Burnt Sienna
                     </button><button
                         onClick={() => setColor("#DA70D6")}
                         className="outline-none px-4 py-1 rounded-full text-black shadow-2xl border-2 border-black"
-                        style={{ backgroundColor: "#DA70D6" }}
+                        style={{
+                            backgroundColor: "#DA70D6",
+                            boxShadow: "0 6px 5px rgba(0, 0, 0, 0.5)" 
+                         }}
                     >
                         Orchid
                     </button><button
                         onClick={() => setColor("#CCCCFF")}
                         className="outline-none px-4 py-1 rounded-full text-black shadow-2xl border-2 border-black"
-                        style={{ backgroundColor: "#CCCCFF" }}
+                        style={{
+                            backgroundColor: "#CCCCFF",
+                            boxShadow: "0 6px 5px rgba(0, 0, 0, 0.5)" 
+                         }}
                     >
                         Periwinkle
                     </button><button
                         onClick={() => setColor("#FA8072")}
                         className="outline-none px-4 py-1 rounded-full text-black shadow-2xl border-2 border-black"
-                        style={{ backgroundColor: "#FA8072" }}
+                        style={{
+                            backgroundColor: "#FA8072",
+                            boxShadow: "0 6px 5px rgba(0, 0, 0, 0.5)" 
+                            
+                            
+                         }}
                     >
                         Salmon
                     </button>
